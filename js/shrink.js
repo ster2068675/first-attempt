43542e5ab40b3c8fdca46221767ff08806ac8169
Sterling <57185391+ster2068675@users.noreply.github.com>
2022-04-18 15:31:47 -0400

    Small updates/tweaks

--- js/shrink.js
+++ js/shrink.js
@@ -2,12 +2,16 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("header").style.fontSize = "20px";
+    document.getElementById("sub").style.fontSize = "10px";
     //add subtitle that also shrinks here 
   } else {
-    document.getElementById("header").style.fontSize = "45px";
+    document.getElementById("header").style.fontSize = "35px";
+    document.getElementById("sub").style.fontSize = "15px";
   }
 }
+
+//Need to add more js functionality ~~ 
 
 //I'll be adding more js soon so the structure will be worked on
